102e13c7aaffbe6d4a2369be1d7a54c301d19701
kathulhur <josephkarl19@outlook.com>
2021-06-15 18:16:24 +0800

    removed the duplicate click events

--- renderers/templateRenderer.js
+++ renderers/templateRenderer.js
@@ -15,39 +15,6 @@ let searchBtn = $('button#searchButton');
 let testBtn = $('a#testButton');
 
 
-// @clar:note --> follow the same setup with the test code that I made below
-/* ----- INDEX BUTTON ----- */
-$('a#indexPageButton').click(function(){
-  event.preventDefault();
-  alert('Go to Index Page!');
-});
-
-/* ----- DATA BUTTON ----- */
-$('a#dataPageButton').click(function(){
-  event.preventDefault();
-  alert('Go to Data Page!');
-});
-
-/* ----- TREE BUTTON ----- */
-$('a#treePageButton').click(function(){
-  event.preventDefault();
-  alert('Go to Tree Page!');
-});
-
-/* ----- SEARCH BUTTON ----- */
-$('button#searchButton').click(function(){
-  event.preventDefault();
-  alert('Search Button - Triggered!');
-});
-
-/* ----- TEST BUTTON ----- */
-$('a#testButton').click(function (event) {
-  event.preventDefault();// @clar:note always add this line inside the function of the click event
-  alert('Test Button - Triggered!');
-  // place your event emitters here
-});
-
-
 // @clar:add --> setup the click event for the "to index page" button
 indexPageBtn.click(function(){
   // @micaela:add --> emit an event for directing the page to the index page
@@ -64,6 +31,8 @@ dataPageBtn.click(function(){
 treePageBtn.click(function(){
   //@micaela:add --> emit an event for directing the page to the tree page
 });
+
+
 
 // @sep,@micai fr @clar :: search button click event [for data page]
 // searchBtn.click(function(){
